4c0bc3f7a2af5ff594a09b59b6cd82e36a1c8cc7
Hamza Aamer <50561910+hamzaaamer@users.noreply.github.com>
2023-04-19 18:03:56 +0500

    ARRAY IN C++

---
+++ Array.cpp
@@ -0,0 +1,37 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <string>
+using namespace std;
+int
+main ()
+{
+
+  cout << " ASHRA MUBASHRA SAHABA NAMES. " << "\n";
+
+  string sahabas[10] =
+    { "\n 1:Hazrat Abu Bakar (R.A) ", 
+    " 2:Hazrat Umer Farooq (R.A) ",
+    " 3:Hazrat Usman (R.A) ", 
+    " 4:Hazrat Ali (R.A) ",
+    " 5:Hazrat Talha (R.A) ",
+    " 6:Hazrat Zubair ibn-e-Awam (R.A) ",
+    " 7:Hazrat Abu Obaida ibn-al-Jarah (R.A) ",
+    " 8:Hazrat Abdul Rehman ibn-e-Auf (R.A) ",
+    " 9:Hazrat Saad ibn-e-Abi Waqas (R.A) ",
+    " 10:Hazrat Saeed ibn-e-Zaid (R.A) "
+  };
+
+  for (int i = 0; i < 10; i++)
+    {
+      cout << sahabas[i] << "\n";
+    }
+
+
+}
